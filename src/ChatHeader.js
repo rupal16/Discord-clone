0ebc738f67182f79ab321d0662c1a3c07f64be62
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-10 01:37:26 +0530

    completed chatHeader component

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,7 +1,37 @@
 import React from "react";
 
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import EditLocationIcon from "@material-ui/icons/EditLocation";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import SearchIcon from "@material-ui/icons/Search";
+import SendIcon from "@material-ui/icons/Send";
+import HelpIcon from "@material-ui/icons/Help";
+import { SendRounded } from "@material-ui/icons";
+
+import "./ChatHeader.css";
+
 function ChatHeader() {
-  return <div className="chatHeader"></div>;
+  return (
+    <div className="chatHeader">
+      <div className="chatHeader__left">
+        <h3>
+          <span className="chatHeader__hash">#</span>Test Channel Name
+        </h3>
+      </div>
+      <div className="chatHeader__right">
+        <NotificationsIcon />
+        <EditLocationIcon />
+        <PeopleAltIcon />
+        <div className="chatHeader__search">
+          <input placeholder="Search" />
+          <SearchIcon />
+        </div>
+
+        <SendIcon />
+        <HelpIcon />
+      </div>
+    </div>
+  );
 }
 
 export default ChatHeader;
