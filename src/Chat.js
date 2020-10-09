6ecafb51c69ceca7e3548e110483f50021524bbf
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-10 01:07:41 +0530

    setup for chatheader component

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ChatHeader from "./ChatHeader";
 
 import "./Chat.css";
 
 function Chat() {
-  return <div className="chat"></div>;
+  return (
+    <div className="chat">
+      <ChatHeader />
+    </div>
+  );
 }
 
 export default Chat;
---
+++ src/ChatHeader.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChatHeader() {
+  return <div className="chatHeader"></div>;
+}
+
+export default ChatHeader;
