80499d46444c4d5b9ab0b466a576c6ba8d37ad9d
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-10 20:47:47 +0530

    created message component

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ChatHeader from "./ChatHeader";
+import Message from "./Message";
+
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import GifIcon from "@material-ui/icons/Gif";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
 
 import "./Chat.css";
 
@@ -7,6 +13,27 @@ function Chat() {
   return (
     <div className="chat">
       <ChatHeader />
+
+      <div className="chat__messages">
+        <Message />
+        <Message />
+        <Message />
+      </div>
+      <div className="chat__input">
+        <AddCircleIcon fontSize="large" />
+        <form>
+          <input placeholder={`Message #TESTCHANNEL`} />
+          <button className="chat__inputButton" type="submit">
+            Send Message
+          </button>
+        </form>
+
+        <div className="chat__inputIcons">
+          <CardGiftcardIcon fontSize="large" />
+          <GifIcon fontSize="large" />
+          <EmojiEmotionsIcon fontSize="large" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,20 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+
+import "./Message.css";
+
+function Message() {
+  return (
+    <div className="message">
+      <Avatar />
+      <div className="message__info">
+        <h4>
+          Rupal<span className="message__timestamp">This is timestamp</span>
+        </h4>
+        <p>This is a message</p>
+      </div>
+    </div>
+  );
+}
+
+export default Message;
