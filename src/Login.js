e2fa762a076eec9dd2de723cc20fa68e2ab4c6bf
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-11 22:24:36 +0530

    designed login component

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,19 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 
 import "./Login.css";
 
 function Login() {
+  const signIn = () => {
+    console.log("signin");
+  };
+
   return (
     <div className="login">
-      <h2>I ma login</h2>
+      <div className="login__logo">
+        <img src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/800px-Discord_logo.svg.png" />
+      </div>
+      <Button onClick={signIn}>Sign In</Button>
     </div>
   );
 }
