ad1631a116e21ddd9a5d5552b1f41b9065ff7f36
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-11 21:51:53 +0530

    integrated redux for authentication

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import Sidebar from "./Sidebar";
 import Chat from "./Chat";
 
 import "./App.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <h4>You nedd to login</h4>
+      )}
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counterSlice";
+import userReducer from "../features/userSlice";
+import appReducer from "../features/appSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
+    app: appReducer,
   },
 });
